bf13ec6064a057a44dfb8211de4f828bc350b73e
Joan Puigcerver <joapuipe@gmail.com>
2018-05-15 01:30:11 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,37 @@
+#!/usr/bin/env python
+
+import os
+
 from setuptools import setup, find_packages
-from torch.utils.ffi import create_extension
 
-phoc_ffi = create_extension(
-    name='prob_phoc._phoc',
-    language='cc',
-    headers=['cpp/phoc.h'],
-    sources=['cpp/phoc.cc'],
-    include_dirs=['cpp'],
-    package=True,
-    relative_to='prob_phoc',
-    extra_compile_args=['-O3', '-march=native', '-fopenmp',
-                        '-std=c++11']).distutils_extension()
+import build
 
-setup(name='prob_phoc', version='0.1', packages=find_packages(),
-      scripts=[],
-      install_requires=['numpy', 'scipy', 'torch'],
-      author='Joan Puigcerver',
-      author_email='joapuipe@gmail.com',
-      license='MIT',
-      url='https://github.com/jpuigcerver/PyLaia',
-      ext_modules=[phoc_ffi])
+this_file = os.path.dirname(__file__)
+
+setup(
+    name='prob_phoc',
+    description='Compute probabilistic PHOC relevance scores efficiently',
+    version='0.1',
+    url='https://github.com/jpuigcerver/prob_phoc',
+    author='Joan Puigcerver',
+    author_email='joapuipe@gmail.com',
+    license='MIT',
+    # Requirements
+    install_requires=[
+        'cffi>=1.0.0',
+        'numpy',
+        'scipy',
+        'torch>=0.3',
+    ],
+    setup_requires=[
+        'cffi>=1.0.0',
+        'torch>=0.3',
+    ],
+    # Exclude the build files.
+    packages=find_packages(exclude=['build']),
+    # Extensions to compile.
+    ext_package='',
+    cffi_modules=[
+        os.path.join(this_file, 'build.py:ffi')
+    ]
+)
